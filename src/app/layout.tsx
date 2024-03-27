107b6d90670355bd207ec3c27618f2dc1767f865
Fadi Alailan <fadialailan@protonmail.com>
2024-03-27 08:21:39 +0300

    added missing gettext function calls and started storing the theme in local storage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,49 @@
 'use client'
-// import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { icon_url } from "@/scripts/global";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import gettext from "@/scripts/gettext";
+import { THEME } from "@/scripts/enums";
+import { useIsMount } from "@/scripts/useIsMount";
 
 const inter = Inter({ subsets: ["latin"] });
-
-// export const metadata: Metadata = {
-//   title: "Porfolio Website",
-//   description: "a porfolio website used to show my porfolio",
-// };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   const [darkEnabled, setDarkEnabled] = useState<boolean>(false);
+  const isMount = useIsMount();
+  const theme_key = 'theme';
+
+  useEffect(() => {
+    if (isMount) {
+      const theme_value = localStorage.getItem(theme_key)
+      switch (theme_value) {
+        case THEME.DARK:
+          setDarkEnabled(true);
+          break;
+        default:
+          setDarkEnabled(false);
+          break;
+      }
+      return;
+    }
+    if (darkEnabled) {
+      localStorage.setItem(theme_key, THEME.DARK);
+    } else {
+      localStorage.setItem(theme_key, THEME.LIGHT);
+    }
+  }, [darkEnabled])
+
   function toggleDarkTheme() {
     setDarkEnabled((darkEnabled) => !darkEnabled)
-    console.log("stuff")
   }
+
   return (
     <html lang="en">
       <head>
@@ -32,13 +53,13 @@ export default function RootLayout({
           href={icon_url}
         />
         <title>Porfolio Website</title>
-        <meta name="description" content="a porfolio website used to show my porfolio"/>
+        <meta name="description" content="a porfolio website used to show my porfolio" />
       </head>
-      <body className={`${inter.className} dark:bg-gray-700 dark:text-white ${darkEnabled && 'dark'}`}>
+      <body dir="ltr" className={`${inter.className} dark:bg-gray-700 dark:text-white ${darkEnabled && 'dark'}`}>
         <nav className="[&>a]:m-2 p-1">
-          <Link href="/">Home</Link>
-          <Link href="/attrubution">Attribution</Link>
-          <button className="float-right p-1 border-black dark:border-white border-2 rounded-s border-solid" onClick={toggleDarkTheme}>dark theme</button>
+          <Link href="/">{gettext.gettext('Home')}</Link>
+          <Link href="/attrubution">{gettext.gettext('Attribution')}</Link>
+          <button className="float-end p-1 border-black dark:border-white border-2 rounded-s border-solid" onClick={toggleDarkTheme}>dark theme</button>
         </nav>
         {children}
       </body>
