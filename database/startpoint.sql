d289dd4805d12264fa8f637a441bf225ff0def14
Fadi Alailan <fadialailan@protonmail.com>
2024-04-02 10:18:52 +0300

    updated the sql startpoint script

--- database/startpoint.sql
+++ database/startpoint.sql
@@ -1,12 +1,21 @@
+-- drop constrains
+ALTER TABLE IF EXISTS project_text DROP CONSTRAINT IF EXISTS project_text_project_id_fkey;
+ALTER TABLE IF EXISTS project_text DROP CONSTRAINT IF EXISTS project_text_language_iso_code_fkey;
+
+-- drop tables
 DROP TABLE IF EXISTS project_text;
 DROP TABLE IF EXISTS project;
 DROP TABLE IF EXISTS language;
+
+-- drop types
 DROP TYPE IF EXISTS text_direction;
 
+-- create types
 CREATE TYPE text_direction AS ENUM ('rtl', 'ltr');
 
+-- create tables
 CREATE TABLE project(
-	id uuid PRIMARY KEY,
+	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
 	image_path text
 );
 
