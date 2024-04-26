21b81e80c6b55df280ce6fa75c836b357542361b
Fadi Alailan <fadialailan@protonmail.com>
2024-04-26 11:05:43 +0300

    improvided the grid style of the ProjectsView component

--- src/components/ProjectsView.tsx
+++ src/components/ProjectsView.tsx
@@ -10,7 +10,9 @@ export interface ProjectViewProps {
 export function ProjectView(props: ProjectViewProps) {
 
   return (
-    <div className="p-4 place-content-around grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-y-8 ">
+    <div className="justify-items-center grid gap-4" 
+      style={{gridTemplateColumns: "repeat(auto-fit, minmax(24rem,1fr))"}}
+    >
       {props.projects_text.map((value, index) => {
         return <ProjectCard key={index} name={value.name} project_id={value.project_id} locale={props.locale} />
       })}
